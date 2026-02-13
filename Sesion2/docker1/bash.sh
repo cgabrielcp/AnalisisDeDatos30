d5a3f27eef9ddd11a716f15a55a8cefffe99561c
Cesarcp <32250045+cgabrielcp@users.noreply.github.com>
2026-02-13 02:14:49 +0000

    deploy docker v2

--- Sesion2/docker1/bash.sh
+++ Sesion2/docker1/bash.sh
@@ -3,3 +3,15 @@ docker build -t image-test .
 
 # listar imagenes
 docker images
+
+# ejecutar un contenedor
+docker run -d -p 8080:8888 --name jupyter image-test
+
+#log de ejecucion del contenedor
+docker logs -f jupyter
+
+# Eliminar una imagen
+docker rmi -f a58859673f21
+
+# Eliminar un contenedor
+docker rm -f 3e47d957070b
